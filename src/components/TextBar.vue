e53c4af86d688ead263d132f2ce1170661de4b58
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-01-16 08:27:05 +0100

    add TextBar component

---
+++ src/components/TextBar.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+type TextBarProps = {
+  caption: string;
+  text: string;
+  isHighlighted?: boolean;
+};
+defineProps<TextBarProps>();
+</script>
+<template>
+  <p class="text-bar" :class="{ '--highlighted': isHighlighted }">
+    <span class="text-bar-caption">{{ caption }}</span>
+    <span class="text-bar-result">{{ text }}</span>
+  </p>
+</template>
+<style scoped>
+.text-bar {
+  width: 17.5rem;
+  height: 2rem;
+  padding: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 0.75rem;
+  background-color: var(--color-neutral-400);
+}
+
+.text-bar-caption {
+  font-size: 0.875rem;
+  font-weight: bold;
+  color: var(--color-neutral-600);
+}
+
+.text-bar-result {
+  font-size: 1.25rem;
+  font-weight: bold;
+  color: var(--color-neutral-700);
+}
+.--highlighted {
+  background-color: var(--color-neutral-800);
+}
+.--highlighted > .text-bar-result,
+.--highlighted > .text-bar-caption {
+  color: var(--color-neutral-200);
+}
+
+@media (min-width: 48em) {
+  .text-bar {
+    width: 34rem;
+    height: 3rem;
+    padding-inline: 2rem;
+    padding-block: 1.5rem;
+  }
+  .text-bar-caption {
+    font-size: 1.125rem;
+  }
+  .text-bar-result {
+    font-size: 2rem;
+  }
+}
+</style>
