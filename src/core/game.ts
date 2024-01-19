0ebd5e81cb01d4d35c3b983674341c78e64ea3cc
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-01-19 18:49:51 +0100

    refactor: move singlePlayerGame fn from src/core.game.ts to src/core/single-player-game.ts

--- src/core/game.ts
+++ src/core/game.ts
@@ -1,8 +1,5 @@
-import { GameOptions, GridSize, Tile } from "./types";
-import { createBoard } from "./board";
-import { ref, reactive } from "vue";
-import timer from "../lib/timer";
-import delayed from "../lib/delayed";
+import { GameOptions } from "./types";
+import singlePlayerGame from "./single-player-game";
 
 export const defaultOptions: GameOptions = {
   players: 1,
@@ -11,95 +8,6 @@ export const defaultOptions: GameOptions = {
   testMode: false,
 };
 
-function singlePlayerGame(grid: GridSize, testMode: boolean) {
-  const gameBoard = createBoard(grid, testMode);
-  const board = reactive(gameBoard.getBoard());
-  const tilesToMatch = grid / 2;
-  const clock = timer();
-
-  let movesMade = ref(0);
-  let matchedTiles = ref(0);
-  let flippedTile: Tile | null = null;
-  let isBoardLocked = false;
-
-  const lockBoard = () => {
-    isBoardLocked = true;
-  };
-  const unlockBoard = () => {
-    isBoardLocked = false;
-  };
-  const getTile = (tileIndex: number) => board[tileIndex];
-  const tilesMatch = (tile1: Tile, tile2: Tile): boolean =>
-    tile1.value === tile2.value;
-  const incrementMoves = () => {
-    movesMade.value += 1;
-  };
-  const incrementScore = () => {
-    matchedTiles.value++;
-  };
-  const markTilesAsMatched = (tile1: Tile, tile2: Tile) => {
-    tile1.isMatched = true;
-    tile2.isMatched = true;
-  };
-  const markTilesAsNotFlipped = (tile1: Tile, tile2: Tile) => {
-    tile1.isFlipped = false;
-    tile2.isFlipped = false;
-  };
-  const handleMatchedTiles = (tile1: Tile, tile2: Tile) => {
-    incrementScore();
-    markTilesAsMatched(tile1, tile2);
-    markTilesAsNotFlipped(tile1, tile2);
-    if (isGameOver()) {
-      clock.stop();
-    }
-  };
-  const handleMismatchedTiles = (tile1: Tile, tile2: Tile) => {
-    markTilesAsNotFlipped(tile1, tile2);
-    flippedTile = null;
-  };
-  const flipTile = (tileIndex: number): void => {
-    if (isBoardLocked) return;
-    if (!clock.hasStarted()) clock.start();
-
-    const tile = getTile(tileIndex);
-    if (tile.isMatched) return;
-    tile.isFlipped = true;
-
-    if (flippedTile) {
-      incrementMoves();
-      if (tilesMatch(tile, flippedTile)) handleMatchedTiles(flippedTile, tile);
-
-      /* skip timeout in tests*/
-      if (testMode) {
-        handleMismatchedTiles(flippedTile, tile);
-      } else {
-        lockBoard();
-        delayed(() => {
-          handleMismatchedTiles(flippedTile!, tile);
-          unlockBoard();
-        }, 1000);
-      }
-    } else {
-      flippedTile = tile;
-    }
-  };
-  const isGameOver = () => matchedTiles.value === tilesToMatch;
-  const restart = () => {
-    clock.reset();
-    gameBoard.reset();
-    movesMade.value = 0;
-    matchedTiles.value = 0;
-  };
-  return {
-    board,
-    getMatchedTiles: () => matchedTiles.value,
-    getMovesMade: () => movesMade.value,
-    flipTile,
-    isGameOver,
-    getTime: () => clock.getTime(),
-    restart,
-  };
-}
 export function createGame(options?: GameOptions) {
   const testMode = options?.testMode || defaultOptions.testMode;
   const grid = options?.grid || defaultOptions.grid;
