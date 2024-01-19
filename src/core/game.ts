9f8badc2e606b9b6114ac4d633ffad616488040e
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-01-19 20:46:40 +0100

    refactor: add type annotation to createGame fn

--- src/core/game.ts
+++ src/core/game.ts
@@ -1,8 +1,8 @@
-import { GameOptions } from "./types";
+import { Game, GameOptions } from "./types";
 import { defaultOptions } from "./options";
 import singlePlayerGame from "./single-player-game";
 
-export function createGame(options?: GameOptions) {
+export function createGame(options?: GameOptions): Game {
   const testMode = options?.testMode || defaultOptions.testMode;
   const grid = options?.grid || defaultOptions.grid;
   return singlePlayerGame(grid, testMode);
