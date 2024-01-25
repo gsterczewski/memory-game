ecd195ea60acae60af2dfbc8e1ad35987949d0f0
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-01-25 07:25:45 +0100

    refactor[core]: move game options from seprate file to core/game

--- src/core/options.ts
+++
@@ -1,9 +0,0 @@
-import { GameOptions } from "./types";
-import { ref, Ref } from "vue";
-export const defaultOptions: GameOptions = {
-  players: 1,
-  grid: 16,
-  theme: "numbers",
-  testMode: false,
-};
-export const options: Ref<GameOptions> = ref({ ...defaultOptions });
