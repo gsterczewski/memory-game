55c86a52230cfeabd6bbb6ae971b12ee1399e204
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-01-26 07:56:35 +0100

    test: add tests for reset method on Board class

--- src/core/board.test.ts
+++ src/core/board.test.ts
@@ -1,40 +1,63 @@
-import { expect, test, describe } from "vitest";
+import { expect, test, describe, beforeEach } from "vitest";
 import { GameBoard } from "./board";
 
+let board16: GameBoard;
+let board36: GameBoard;
+beforeEach(() => {
+  board16 = new GameBoard(16);
+  board36 = new GameBoard(36);
+});
 describe("GameBoard", () => {
   test("generates 4x4 game board", () => {
-    const board = new GameBoard(16);
-    expect(board.getTiles().length).toEqual(16);
+    expect(board16.getTiles().length).toEqual(16);
   });
   test("generates 6x6 game board", () => {
-    const board = new GameBoard(36);
-    expect(board.getTiles().length).toEqual(36);
+    expect(board36.getTiles().length).toEqual(36);
   });
 
   test("GameBoard.shuffle() shuffles tiles ", () => {
-    const board = new GameBoard(36);
     const board2 = new GameBoard(36);
-    board.shuffle();
+    board36.shuffle();
     board2.shuffle();
     const order = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
-    const values1 = board.getTiles().map((tile) => tile.getValue());
+    const values1 = board36.getTiles().map((tile) => tile.getValue());
     const values2 = board2.getTiles().map((tile) => tile.getValue());
     expect(values1).not.toEqual(order);
     expect(values2).not.toEqual(values1);
   });
   test("GameBoard.getTiles() returns array with correct number of tiles", () => {
-    const tiles16 = new GameBoard(16).getTiles();
-    const tiles36 = new GameBoard(36).getTiles();
-    expect(tiles16.length).toEqual(16);
-    expect(tiles36.length).toEqual(36);
+    expect(board16.getTiles().length).toEqual(16);
+    expect(board36.getTiles().length).toEqual(36);
   });
   test("GameBoard.getTile(index) returns correct tile", () => {
-    const board = new GameBoard(16);
-    const expectedTile1Value = 1;
-    const expectedTile2Value = 8;
-    const tile1 = board.getTile(0);
-    const tile2 = board.getTile(15);
-    expect(tile1.getValue()).toEqual(expectedTile1Value);
-    expect(tile2.getValue()).toEqual(expectedTile2Value);
+    const tilesToCheck = [0, 8, 9, 15];
+    const expectedValues = [1, 1, 2, 8];
+    tilesToCheck.forEach((tile, index) => {
+      expect(board16.getTile(tile).getValue()).toEqual(expectedValues[index]);
+    });
+  });
+  describe("reset()", () => {
+    test("sets 'isFlipped' prop on all tiles to false", () => {
+      const tilesToFlip = [0, 8, 9, 15];
+      tilesToFlip.forEach((tile) => {
+        board16.getTile(tile).markAsFlipped();
+      });
+      expect(board16.getTiles().some((tile) => tile.isFlipped)).toEqual(true);
+      board16.reset();
+      expect(
+        board16.getTiles().every((tile) => tile.isFlipped === false)
+      ).toEqual(true);
+    });
+    test("sets 'isMatched' prop on all tiles to false", () => {
+      const tilesToMatch = [0, 8, 9, 15];
+      tilesToMatch.forEach((tile) => {
+        board16.getTile(tile).markAsMatched();
+      });
+      expect(board16.getTiles().some((tile) => tile.isMatched)).toEqual(true);
+      board16.reset();
+      expect(
+        board16.getTiles().every((tile) => tile.isMatched === false)
+      ).toEqual(true);
+    });
   });
 });
