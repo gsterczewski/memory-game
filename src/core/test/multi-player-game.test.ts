662845b8b283a0d6df4eec4399c7783b6c369914
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-01-27 10:43:18 +0100

    test: replace tests.todos with tests for MultiPlayerGame

--- src/core/test/multi-player-game.test.ts
+++ src/core/test/multi-player-game.test.ts
@@ -1,6 +1,7 @@
 import { expect, test, describe, beforeEach } from "vitest";
 import { MultiPlayerGame } from "../multi-player-game";
 import { GameOptions } from "../game";
+import { selectTilesAsync } from "./helpers";
 const testOptions: GameOptions = {
   players: 2,
   order: "sequential",
@@ -18,42 +19,135 @@ beforeEach(() => {
 });
 
 describe("multiplayer game", () => {
-  describe.todo("getActivePlayerID", () => {
+  describe("getActivePlayerID", () => {
     describe("2 player game", () => {
-      test("after 0 moves, returns id of player 1", async () => {});
-      test("after 1 move, returns id of player 2", async () => {});
-      test("after 2 moves, returns id of player 1", async () => {});
-      test("after 3 moves, returns id of player 2", async () => {});
-      test("after even n of moves, returns id of player 1", async () => {});
-      test("after odd n of moves, returns id of player 2", async () => {});
+      test("after 0 moves, returns id of player 1", () => {
+        expect(game2.getActivePlayerID()).toEqual("player-1");
+      });
+      test("after 1 move, returns id of player 2", async () => {
+        await selectTilesAsync([0, 1], game2);
+        expect(game2.getActivePlayerID()).toEqual("player-2");
+      });
+      test("after 2 moves, returns id of player 1", async () => {
+        await selectTilesAsync([0, 1, 5, 6], game2);
+        expect(game2.getActivePlayerID()).toEqual("player-1");
+      });
+      test("after 3 moves, returns id of player 2", async () => {
+        await selectTilesAsync([0, 1, 5, 6, 7, 8], game2);
+        expect(game2.getActivePlayerID()).toEqual("player-2");
+      });
+      test("after even n of moves, returns id of player 1", async () => {
+        await selectTilesAsync([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], game2);
+        expect(game2.getActivePlayerID()).toEqual("player-1");
+      });
+      test("after odd n of moves, returns id of player 2", async () => {
+        await selectTilesAsync(
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+          game2
+        );
+        expect(game2.getActivePlayerID()).toEqual("player-2");
+      });
     });
     describe("3 player game", () => {
-      test("after 0 moves, returns id of player 1", async () => {});
-      test("after 1 move, returns id of player 2", async () => {});
-      test("after 2 moves, returns id of player 3", async () => {});
-      test("after [3,6,9] moves, returns id of player 1", async () => {});
-      test("after [4,7,10] moves, returns id of player 2", async () => {});
-      test("after [5,8,11] moves, returns id of player 3", async () => {});
+      test("after 0 moves, returns id of player 1", async () => {
+        expect(game3.getActivePlayerID()).toEqual("player-1");
+      });
+      test("after 1 move, returns id of player 2", async () => {
+        await selectTilesAsync([0, 1], game3);
+        expect(game3.getActivePlayerID()).toEqual("player-1");
+      });
+      test("after 2 moves, returns id of player 3", async () => {
+        await selectTilesAsync([0, 1, 2, 3], game3);
+        expect(game3.getActivePlayerID()).toEqual("player-2");
+      });
+      test("after 6 moves, returns id of player 1", async () => {
+        await selectTilesAsync([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], game3);
+        expect(game3.getActivePlayerID()).toEqual("player-1");
+      });
+      test("after 7 moves, returns id of player 2", async () => {
+        await selectTilesAsync(
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+          game3
+        );
+        expect(game3.getActivePlayerID()).toEqual("player-2");
+      });
+      test("after 8 moves, returns id of player 3", async () => {
+        await selectTilesAsync(
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+          game3
+        );
+        expect(game3.getActivePlayerID()).toEqual("player-3");
+      });
     });
     describe("4 player game", () => {
-      test("after 0 moves, returns id of player 1", async () => {});
-      test("after 1 move, returns id of player 2", async () => {});
-      test("after 2 moves, returns id of player 3", async () => {});
-      test("after 3 moves, returns id of player 4", async () => {});
-      test("after [4,8,12] moves, returns id of player 1", async () => {});
-      test("after [5,9,13] moves, returns id of player 2", async () => {});
-      test("after [6,10,14] moves, returns id of player 3", async () => {});
-      test("after [7,11,15] moves, returns id of player 4", async () => {});
+      test("after 0 moves, returns id of player 1", async () => {
+        expect(game4.getActivePlayerID()).toEqual("player-1");
+      });
+      test("after 1 move, returns id of player 2", async () => {
+        await selectTilesAsync([0, 1], game4);
+        expect(game4.getActivePlayerID()).toEqual("player-2");
+      });
+      test("after 2 moves, returns id of player 3", async () => {
+        await selectTilesAsync([0, 1, 2, 3], game4);
+        expect(game4.getActivePlayerID()).toEqual("player-3");
+      });
+      test("after 3 moves, returns id of player 4", async () => {
+        await selectTilesAsync([0, 1, 2, 3, 4, 5], game4);
+        expect(game4.getActivePlayerID()).toEqual("player-4");
+      });
+      test("after 8 moves, returns id of player 1", async () => {
+        await selectTilesAsync(
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+          game4
+        );
+        expect(game4.getActivePlayerID()).toEqual("player-1");
+      });
+      test("after 9 moves, returns id of player 2", async () => {
+        await selectTilesAsync(
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 1],
+          game4
+        );
+        expect(game4.getActivePlayerID()).toEqual("player-2");
+      });
+      test("after 10 moves, returns id of player 3", async () => {
+        await selectTilesAsync(
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3],
+          game4
+        );
+        expect(game4.getActivePlayerID()).toEqual("player-3");
+      });
+      test("after 11 moves, returns id of player 4", async () => {
+        await selectTilesAsync(
+          [
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 5,
+            6,
+          ],
+          game4
+        );
+        expect(game4.getActivePlayerID()).toEqual("player-4");
+      });
     });
   });
   describe("getScores()", () => {
-    test.todo(
-      "when game has not yet started, it should return n-sized array of 0's, where n = number of players",
-      () => {}
-    );
-    test.todo(
-      "when game has started, it should return n-sized array of numbers equal to each player score "
-    );
+    test("when game has not yet started, it should return n-sized array of 0's, where n = number of players", () => {
+      expect(game4.getScores()).toEqual([0, 0, 0, 0]);
+    });
+    test("when game has started, it should return n-sized array of numbers equal to each player score ", async () => {
+      await selectTilesAsync([0, 8], game4);
+      expect(game4.getScores()).toEqual([1, 0, 0, 0]);
+      await selectTilesAsync([1, 9], game4);
+      expect(game4.getScores()).toEqual([1, 1, 0, 0]);
+      await selectTilesAsync([2, 10], game4);
+      expect(game4.getScores()).toEqual([1, 1, 1, 0]);
+      await selectTilesAsync([3, 11], game4);
+      expect(game4.getScores()).toEqual([1, 1, 1, 1]);
+      await selectTilesAsync([4, 12], game4);
+      expect(game4.getScores()).toEqual([2, 1, 1, 1]);
+      await selectTilesAsync([5, 15], game4);
+      expect(game4.getScores()).toEqual([2, 1, 1, 1]);
+      await selectTilesAsync([5, 13], game4);
+      expect(game4.getScores()).toEqual([2, 1, 2, 1]);
+    });
   });
   describe("reset()", () => {
     test.todo("it should reset game board to initial state", () => {});
