418899e0914f3597493f4f32ada12ba964bdf6a8
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-01-27 07:42:29 +0100

    refactor: adapt SinglePlayerGame to use GameEngine for evaluation

--- src/core/single-player-game.ts
+++ src/core/single-player-game.ts
@@ -1,49 +1,26 @@
 import { Timer } from "../lib/timer";
 import { Game, GameOptions, SPGameResults } from "./game";
-import { Tile } from "./tile";
 import { Player } from "./player";
-import delayed from "../lib/delayed";
+import { GameEngine } from "./game-engine";
 
 export class SinglePlayerGame extends Game {
-  private readonly MS_IN_SECOND = 1000;
   private timer: Timer;
-  private previousTile: Tile | undefined;
   private player: Player;
   private tilesToMatch: number;
+  private engine: GameEngine;
   constructor(options: GameOptions) {
     super(options);
     this.timer = new Timer();
     this.tilesToMatch = options.boardSize / 2;
     this.player = new Player("player-1");
-  }
-
-  private incrementMoves() {
-    this.player.incrementMoves();
+    this.engine = new GameEngine(options.gameDelay);
   }
 
   private areAllTilesMatched(): boolean {
     return this.player.getScore() === this.tilesToMatch;
   }
-  private handleMatchedTiles(tile1: Tile, tile2: Tile) {
-    this.player.incrementScore();
-    tile1.markAsMatched();
-    tile2.markAsMatched();
-    tile1.markAsNotFlipped();
-    tile2.markAsNotFlipped();
-    this.previousTile = undefined;
-    if (this.areAllTilesMatched()) {
-      this.stop(() => {
-        this.timer.stop();
-      });
-    }
-  }
-  private handleMismatchedTiles(tile1: Tile, tile2: Tile) {
-    tile1.markAsNotFlipped();
-    tile2.markAsNotFlipped();
-    this.previousTile = undefined;
-  }
 
-  public selectTile(tileIndex: number): Tile | undefined {
+  public async selectTile(tileIndex: number) {
     if (tileIndex < 0 || tileIndex > this.getBoard().length - 1) return;
     if (this.isGameLocked) return;
     if (this.currentGameStatus !== this.GAME_STATUS.STARTED) {
@@ -51,32 +28,18 @@ export class SinglePlayerGame extends Game {
         this.timer.start();
       });
     }
-
-    const tile = this.getTile(tileIndex);
-    if (this.previousTile?.getID() === tile.getID()) return;
-    if (tile.isMatched) return;
-
-    tile.markAsFlipped();
-
-    if (this.previousTile) {
-      this.incrementMoves();
-      if (this.areTilesEqual(tile, this.previousTile)) {
-        this.handleMatchedTiles(tile, this.previousTile);
-      } else {
-        if (this.gameSpeed === 0) {
-          this.handleMismatchedTiles(tile, this.previousTile);
-        } else {
-          this.lockGame();
-          delayed(() => {
-            this.handleMismatchedTiles(tile, this.previousTile!);
-            this.unlockGame();
-          }, this.MS_IN_SECOND * this.gameSpeed);
-        }
-      }
-    } else {
-      this.previousTile = tile;
+    const { moves, score } = await this.engine.setTile(this.getTile(tileIndex));
+    if (moves) {
+      this.player.incrementMoves();
     }
-    return tile;
+    if (score) {
+      this.player.incrementScore();
+    }
+    if (this.areAllTilesMatched()) {
+      this.stop(() => {
+        this.timer.stop();
+      });
+    }
   }
 
   public getMoves() {
@@ -91,6 +54,7 @@ export class SinglePlayerGame extends Game {
   public reset(): void {
     this.timer.reset();
     this.player.reset();
+    this.board.reset();
     this.setGameStatus(this.GAME_STATUS.NOT_STARTED);
   }
 
