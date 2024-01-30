52aa6959339f412785cd57fa7554a764d20113f0
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-01-30 10:20:54 +0100

    refactor: extract .overlay css class from PopupMenu and turn it into component for reuse

---
+++ src/components/AppOverlay.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="overlay">
+    <slot></slot>
+  </div>
+</template>
+<style scoped>
+.overlay {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  z-index: 999;
+  overflow: hidden;
+  display: grid;
+  place-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+</style>
--- src/components/PopupMenu.vue
+++ src/components/PopupMenu.vue
@@ -8,29 +8,16 @@ type PopupMenuProps = {
 defineProps<PopupMenuProps>();
 </script>
 <template>
-  <div class="overlay">
-    <div class="overlay-box">
-      <BaseButton @click="handleRestart" theme="primary" size="block"
-        >Restart</BaseButton
-      >
-      <BaseButton @click="handleNewGame" size="block">New Game</BaseButton>
-      <BaseButton @click="handleResume" size="block">Resume Game</BaseButton>
-    </div>
+  <div class="menu-box">
+    <BaseButton @click="handleRestart" theme="primary" size="block"
+      >Restart</BaseButton
+    >
+    <BaseButton @click="handleNewGame" size="block">New Game</BaseButton>
+    <BaseButton @click="handleResume" size="block">Resume Game</BaseButton>
   </div>
 </template>
 <style scoped>
-.overlay {
-  width: 100%;
-  height: 100%;
-  position: fixed;
-  top: 0;
-  z-index: 999;
-  overflow: hidden;
-  display: grid;
-  place-items: center;
-  background-color: rgba(0, 0, 0, 0.5);
-}
-.overlay-box {
+.menu-box {
   width: min(87%, 450px);
   aspect-ratio: 1.5/1;
   display: flex;
