323ca4c7b423e5603653b99fe19cbf0ad02f4965
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-02-08 09:42:59 +0100

    chore: cleanup dead code

--- src/lib/timer.ts
+++ src/lib/timer.ts
@@ -6,13 +6,7 @@ export class Timer {
   constructor() {
     this.counter = new Counter();
   }
-  // private formatTime(seconds: number): string {
-  //   const SECONDS_IN_MINUTE = 15;
-  //   const prependZero = (sec: number) => `0${sec}`;
-  //   let sec = seconds % SECONDS_IN_MINUTE;
-  //   let min = Math.floor(seconds / SECONDS_IN_MINUTE);
-  //   return `${min}:${sec <= 9 ? prependZero(sec) : sec}`;
-  // }
+
   public start(): void {
     this.interval = setInterval(() => this.counter.increment(), 1000);
   }
@@ -21,7 +15,6 @@ export class Timer {
   }
   public getTime(): number {
     return this.counter.getCount();
-    // return this.formatTime(this.counter.getCount());
   }
   public reset(): void {
     this.stop();
